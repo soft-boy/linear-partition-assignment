1168598ecc31567ca608394aa7822c874dea2aa9
Eric Wang <ewang1997@gmail.com>
2024-06-25 00:34:14 -0700

    submit 3

--- 1_mfe.py
+++ 1_mfe.py
@@ -8,55 +8,63 @@ summarize = lambda x, y: max(x, y)
 combine = lambda x, y: x*y
 semiring=(phi, summarize, combine, 0, 1)
 
+def test(rna, secondary):
+  Q = linear_partition(rna, b=100, semiring=semiring, T=T)
+  mfe = calculate_mfe(rna, secondary, T)
+
+  print(rna, secondary)
+  print(Q[-1][1], mfe)
+  print()
+
 class TestInside(unittest.TestCase):
   def test_1(self):
     rna, secondary = "ACAGU", '((.))'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
 
   def test_2(self):
     rna, secondary = "AC", '..'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
   
   def test_3(self):
     rna, secondary = "GUAC", '(())'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
   
   def test_4(self):
     rna, secondary = "GCACG", '().()'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
 
   def test_5(self):
     rna, secondary = "CCGG", '(())'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
   
   def test_6(self):
     rna, secondary = "CCCGGG", '((()))'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
   
   def test_7(self):
     rna, secondary = "UUCAGGA", '(((.)))'
     
     Q = linear_partition(rna, b=100, semiring=semiring, T=T)
     mfe = calculate_mfe(rna, secondary, T)
-    self.assertEqual(round(Q[-1][1]), round(mfe))
+    self.assertAlmostEqual(Q[-1][1], mfe, delta=0.001)
 
 if __name__ == '__main__':
   unittest.main()
